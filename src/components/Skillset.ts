2a331fffd7140e09f5212ac67a880cb313cdb956
Niko Hujanen <niko.hujanen@gmail.com>
2026-02-19 09:09:41 +0200

    fix: min-w

--- src/components/Skillset.ts
+++ src/components/Skillset.ts
@@ -7,7 +7,7 @@ export default function Skillset(key: string, value: string, className: string =
             class: `flex flex-row gap-3 mb-2 ${className} max-sm:flex-col max-sm:gap-0`,
         },
         createElement('dt', {"class": "font-bold whitespace-nowrap"}, key),
-        createElement('div', {"class": "max-sm:hidden min-w-[3em] flex-auto after:border-b-2 after:border-gray-400 after:border-dotted after:block after:h-1 after:w-full after:translate-y-[250%]"}, ''),
+        createElement('div', {"class": "max-sm:hidden min-w-[18em] flex-auto after:border-b-2 after:border-gray-400 after:border-dotted after:block after:h-1 after:w-full after:translate-y-[250%]"}, ''),
         createElement('dd', {"class": ""}, value),
     );
 }
